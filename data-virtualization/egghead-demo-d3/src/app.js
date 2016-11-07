1146265bebfa5004181f65dc8781fcff535bdc45
suzp1984 <suzp1984@gmail.com>
2016-11-07 18:13:30 +0800

    add egghead's d3 demo

---
+++ data-virtualization/egghead-demo-d3/src/app.js
@@ -0,0 +1,62 @@
+(function() {
+    let scores = [
+        {name: 'Alice', score: 96},
+        {name: 'Billy', score: 83},
+        {name: 'Cindy', score: 91},
+        {name: 'David', score: 96},
+        {name: 'Emily', score: 88}
+    ];
+
+    function scaleBar(selection, scale) {
+        selection.style('transform', 'scaleX(' + scale + ')');
+    }
+
+    function setFill(selection, color) {
+        selection.style('fill', color);
+    }
+    
+    function fade(selection, opacity) {
+        selection.style('fill-opacity', opacity);
+    }
+    
+    let bar = d3.select('.chart')
+            .append('svg')
+              .attr('width', 225)
+              .attr('height', 300)
+            .selectAll('g')
+            .data(scores)
+            .enter()
+              .append('g')
+              .attr('transform', (d, i) => 'translate(0, ' + i * 33 + ')');
+
+    bar.append('rect')
+        .style('width', d => d.score)
+        .attr('class', 'bar')
+        .on('mouseover', function(d, i, elements) {
+            d3.select(this)
+                .call(scaleBar, 2)
+                .call(setFill, 'teal');
+
+            d3.selectAll(elements)
+                .filter(':not(hover)')
+                .call(fade, 0.5);
+
+        })
+        .on('mouseout', function(d, i, elements) {
+            d3.select(this)
+                .call(scaleBar, 1)
+                .call(setFill, 'lightgreen');
+
+            d3.selectAll(elements)
+                .call(fade, 1);
+        });
+
+    bar.append('text')
+        .attr('y', 20)
+        .text(function(d) {
+            return d.name;
+        });
+    
+})();
+
+
