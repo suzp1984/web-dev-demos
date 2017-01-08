36e32475aaff7cf52a16ffdf2173965990a8eb61
suzp1984 <suzp1984@gmail.com>
2017-01-08 21:53:43 +0800

    make gallery rotate random

---
+++ demos/gallery/js/data.js
@@ -0,0 +1,82 @@
+window.data = [
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandma.JPG"
+    },
+    {caption: "grandparents",
+     desc:    "grand parents",
+     img :    "image/grandparent1.JPG"
+    },
+    {caption: "grandparents",
+     desc:    "grandparents",
+     img :    "image/grandparent2.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandma.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandparent1.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandma.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandma.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandparent1.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandma.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandma.JPG"
+    },
+        {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandparent1.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandma.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandma.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandparent1.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandma.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandma.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandparent1.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandma.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandparent1.JPG"
+    },
+    {caption: "grandma",
+     desc:    "grandma",
+     img :    "image/grandma.JPG"
+    }
+];
--- demos/gallery/js/main.js
+++ demos/gallery/js/main.js
@@ -1,13 +1,122 @@
 (function() {
+
+    function g(selector) {
+        let method = selector.substr(0, 1) == '.' ?
+                'getElementsByClassName' : 'getElementById';
+
+        return document[method](selector.substr(1));
+    }
+
+    let data = window.data;
+    function addPhotos() {
+        let template = g('#wrap').innerHTML;
+        let html = [];
+        for (let s in data) {
+            let _html = template.replace('{{index}}', s)
+                    .replace('{{img}}', data[s].img)
+                    .replace('{{caption}}', data[s].caption)
+                    .replace('{{desc}}', data[s].desc);
+            html.push(_html);
+        }
+
+
+        g('#wrap').innerHTML = html.join('');
+
+        rsort(random([0, data.length - 1]));
+    }
+
+    function random(range) {
+        let max = Math.max(range[0], range[1]);
+        let min = Math.min(range[0], range[1]);
+
+        let diff = max - min;
+        let number = Math.ceil(Math.random() * diff + min);
+
+        return number;
+    }
+
+    addPhotos();
+
+    function range() {
+        let range = {
+            left: { x: [], y: [] },
+            right: { x: [], y: [] }
+        };
+
+        let wrap = {
+            w : g('#wrap').clientWidth,
+            h : g('#wrap').clientHeight
+        };
+
+        let photo = {
+            w : g('.photo')[0].clientWidth,
+            h : g('.photo')[0].clientHeight
+        };
+
+        console.log(wrap);
+        console.log(photo);
+
+        range.left.x = [ 0 - photo.w, wrap.w/2 - photo.w/2 ];
+        range.left.y = [ 0 - photo.h, wrap.h ];
+        range.right.x = [ wrap.w/2 + photo.w/2, wrap.w + photo.w ];
+        range.right.y = [ 0 - photo.h, wrap.h ];
+
+        return range;
+    }
+    
+    function rsort(n) {
+
+        let _photo = g('.photo');
+        let photos = [];
+        
+        for (s = 0; s < _photo.length; s++) {
+            _photo[s].className = _photo[s].className.replace(/\s*photo-center\s*/, ' ');
+            photos.push(_photo[s]);
+        }
+        
+        let photo_center = g('#photo_' + n);
+        photo_center.className += ' photo-center';
+        photo_center = photos.splice(n, 1)[0];
+
+        let photos_left = photos.splice(0, Math.ceil(photos.length/2));
+        let photos_right = photos;
+
+        let ranges = range();
+        
+        for (s in photos_left) {
+            let photo = photos_left[s];
+
+            photo.style.left = random(ranges.left.x) + 'px';
+            photo.style.top = random(ranges.left.y) + 'px';
+
+            photo.style['-webkit-transform'] = 'rotate(' + random([-45, 45]) + 'deg)';
+        }
+
+        for (s in photos_right) {
+            let photo = photos_right[s];
+
+            photo.style.left = random(ranges.right.x) + 'px';
+            photo.style.top = random(ranges.right.y) + 'px';
+
+            photo.style['-webkit-transform'] = 'rotate(' + random([-45, 45]) + 'deg)';
+        }
+        
+    }
+    
     window.turn = 
     function (elem) {
         let cls = elem.className;
         if (/photo-front/.test(cls)) {
-            cls = cls.replace(/photo-front/, ' photo-back');
+            cls = cls.replace(/photo-front/, 'photo-back');
         } else {
-            cls = cls.replace(/photo-back/, ' photo-front');
+            cls = cls.replace(/photo-back/, 'photo-front');
         }
 
         elem.className = cls;
     };
+
+    
 })();
+
+
+
